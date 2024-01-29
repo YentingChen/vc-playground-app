f6c0e3de5acef430993146a1faa401ccdb7e2fe7
Yenting Chen <yenting.chen@ziffmedia.com>
2024-01-29 06:50:07 +0000

    native-module

---
+++ expo-settings/src/index.ts
@@ -0,0 +1,24 @@
+import { EventEmitter, Subscription } from "expo-modules-core";
+import ExpoSettingsModule from "./ExpoSettingsModule";
+
+const emitter = new EventEmitter(ExpoSettingsModule);
+
+export type Theme = "light" | "dark" | "system";
+
+export type ThemeChangeEvent = {
+  theme: Theme;
+};
+
+export function addThemeListener(
+  listener: (event: ThemeChangeEvent) => void
+): Subscription {
+  return emitter.addListener<ThemeChangeEvent>("onChangeTheme", listener);
+}
+
+export function getTheme(): string {
+  return ExpoSettingsModule.getTheme();
+}
+
+export function setTheme(theme: string): void {
+  return ExpoSettingsModule.setTheme(theme);
+}
