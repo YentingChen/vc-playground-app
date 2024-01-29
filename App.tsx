89d3e6c4c26942de2d17060ecf2e9d376db59de4
Yenting Chen <yenting.chen@ziffmedia.com>
2024-01-29 03:00:09 +0000

    clean project

--- App.tsx
+++ App.tsx
@@ -1,19 +1,15 @@
-import { useEffect } from "react";
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
-import Braze from "react-native-appboy-sdk";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
 
 export default function App() {
-  useEffect(() => {
-    Braze.changeUser("494efe606b75b1");
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>This is an internal app to test out various technologies and experiments</Text>
+      <Text>
+        This is an internal app to test out various technologies and experiments
+      </Text>
       <Button
         title="Press me"
-        onPress={() => Alert.alert('Simple Button pressed')}
+        onPress={() => Alert.alert("Simple Button pressed")}
       />
       <StatusBar style="auto" />
     </View>
@@ -23,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
