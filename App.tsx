a860098686bbafb3277ad825469585457381b5a4
Yenting Chen <yenting.chen@ziffmedia.com>
2024-05-17 10:37:10 +0100

    native module integration

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button, Alert } from "react-native";
+import * as Settings from "./modules/my-module";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>
+        {Settings.hello()}
         This is an internal app to test out various technologies and experiments
       </Text>
       <Button
